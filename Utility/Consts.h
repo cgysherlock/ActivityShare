363154b729ba9756b3db1119d6e94a534efcc6c0
cgysherlock <251879836@qq.com>
2018-03-14 10:28:48 +0800

    update pod

---
+++ Utility/Consts.h
@@ -0,0 +1,13 @@
+//
+//  Consts.h
+//  ActivitySharing
+//
+//  Created by sherlock on 2018/3/12.
+//  Copyright © 2018年 cgy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Consts : NSObject
+
+@end
---
+++ Utility/Toast/Toast.m
@@ -0,0 +1,13 @@
+//
+//  Toast.m
+//  ActivitySharing
+//
+//  Created by sherlock on 2018/3/12.
+//  Copyright © 2018年 cgy. All rights reserved.
+//
+
+#import "Toast.h"
+
+@implementation Toast
+
+@end
